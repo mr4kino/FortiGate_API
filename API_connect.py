7bbc6c8e2f0ddef9ea1bdf031a3cf34535497ec5
mr4kino <30932864+mr4kino@users.noreply.github.com>
2017-08-21 20:09:36 +0100

    Update API_connect.py
    Added the right csrftoken into the header.

--- API_connect.py
+++ API_connect.py
@@ -10,28 +10,16 @@ password = 'abcdefg'
 #all cookies received will be stored in the session object
 client = requests.session() 
 
-print 'client headers initially: ', client.headers, '\n \n'
-print 'client cookies initially: ', client.cookies, '\n \n'
 
 #First connection used for authentication.
 login = client.post(url + '/logincheck', data="username=" + name + "&secretkey=" + password, verify = False)
 
 #csrftoken to be inserted in the headers for next put,post,delete requests. This will be stored in csrftoken variable. 
 #On the FGT it is named ccsrftoken (double c)
-#csrftoken = client.cookies['ccsrftoken']
-print 'client cookies after login: ', client.cookies, '\n \n'
-print 'csrftoken value extracted from the cookie: ', client.cookies['ccsrftoken'], '\n \n'
+ini = client.cookies['ccsrftoken']
+csrftoken = ini[1:-1] # this is to remove the double quote
 
-#for initial debugging: we print the values that has been stored for the crsftoken value.
-#print csrftoken
-#print client.cookies
-
-#for debugging, we print the headers variable value
-#print headers
-#print '\n \n \n'
-
-client.headers.update({"X-CSRFTOKEN": client.cookies['ccsrftoken']})
-print client.headers
+client.headers.update({"X-CSRFTOKEN": csrftoken})
 
 
 get_cmdb = client.get(url + 'api/v2/cmdb/firewall/address?vdom=root', verify = False)
@@ -41,13 +29,6 @@ print get_cmdb.text
 
 print '\n'
 
-#we try to post, ie create, a firewall address
-addresstest = {'name':"address13", 'type': "ipmask", 'subnet': "1.1.1.0 255.255.255.0"}
-api_cmdb = 'api/v2/cmdb/'
-c = client.post(url + api_cmdb + 'firewall/address?vdom=root', params= addresstest, verify = False)
-print '\n'
-#print c.text
-
 
 client.close()
-#print b
+
